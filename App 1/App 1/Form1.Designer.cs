087786bcda34c721bc809442faba3f28880ed389
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-09-11 19:34:15 +0530

    making app1

---
+++ App 1/App 1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace App_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox01 = new System.Windows.Forms.TextBox();
+            this.averageDowntimeTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(178, 206);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(146, 22);
+            this.textBox4.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(178, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(178, 178);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(146, 22);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(178, 234);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(146, 22);
+            this.textBox5.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Downtime 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Downtime 1";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(74, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Downtime 1";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(74, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Downtime 4";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(74, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Downtime 3";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(74, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Downtime 2";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(74, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Downtime 5";
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(105, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(204, 32);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Downtime Calc";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(111, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Create Downtime Array";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(481, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Display Average Downtime";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(446, 133);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 25);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Output";
+            this.label9.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(498, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox01
+            // 
+            this.textBox01.Location = new System.Drawing.Point(178, 122);
+            this.textBox01.Name = "textBox01";
+            this.textBox01.Size = new System.Drawing.Size(146, 22);
+            this.textBox01.TabIndex = 0;
+            this.textBox01.TextChanged += new System.EventHandler(this.textBox01_TextChanged);
+            // 
+            // averageDowntimeTextBox
+            // 
+            this.averageDowntimeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageDowntimeTextBox.Location = new System.Drawing.Point(481, 178);
+            this.averageDowntimeTextBox.Multiline = true;
+            this.averageDowntimeTextBox.Name = "averageDowntimeTextBox";
+            this.averageDowntimeTextBox.Size = new System.Drawing.Size(243, 176);
+            this.averageDowntimeTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.averageDowntimeTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox01);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox01;
+        private System.Windows.Forms.TextBox averageDowntimeTextBox;
+    }
+}
+
---
+++ App 1/App 1/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_1
+{
+    public partial class Form1 : Form
+    {
+        private float[] serverDowntimes = new float[5];
+        private bool[] downtimeEntered = new bool[5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Check if any of the server downtime values are empty
+            /* if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(textBox2.Text) ||
+                string.IsNullOrWhiteSpace(textBox3.Text) ||
+                string.IsNullOrWhiteSpace(textBox4.Text) ||
+                string.IsNullOrWhiteSpace(textBox5.Text))
+            {
+                MessageBox.Show("Please enter values for all server downtime fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Store inputted server downtime values in the array
+            serverDowntimes[0] = float.Parse(textBox1.Text);
+            serverDowntimes[1] = float.Parse(textBox2.Text);
+            serverDowntimes[2] = float.Parse(textBox3.Text);
+            serverDowntimes[3] = float.Parse(textBox4.Text);
+            serverDowntimes[4] = float.Parse(textBox5.Text);
+            
+          //  MessageBox.Show("Exam scores array created successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            
+
+            // Sort the server downtime values
+            float[] sortedDowntimes = serverDowntimes.OrderBy(d => d).ToArray();
+
+            // Display sorted downtime values
+            sortedDowntimesTextBox.Clear();
+            foreach (var downtime in sortedDowntimes)
+            {
+                sortedDowntimesTextBox.AppendText(downtime.ToString() + Environment.NewLine);
+            }
+            */
+            // Check if each downtime value is entered
+            if (string.IsNullOrWhiteSpace(textBox01.Text))
+            {
+                MessageBox.Show("Please enter value for Downtime 1.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                serverDowntimes[0] = float.Parse(textBox01.Text);
+                downtimeEntered[0] = true;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Please enter value for Downtime 2.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                serverDowntimes[1] = float.Parse(textBox2.Text);
+                downtimeEntered[1] = true;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("Please enter value for Downtime 3.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                serverDowntimes[2] = float.Parse(textBox3.Text);
+                downtimeEntered[2] = true;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                MessageBox.Show("Please enter value for Downtime 4.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                serverDowntimes[3] = float.Parse(textBox4.Text);
+                downtimeEntered[3] = true;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox5.Text))
+            {
+                MessageBox.Show("Please enter value for Downtime 5.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                serverDowntimes[4] = float.Parse(textBox5.Text);
+                downtimeEntered[4] = true;
+            }
+
+            MessageBox.Show("Server downtime values stored successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check if any downtime value is not entered
+            for (int i = 0; i < 5; i++)
+            {
+                if (!downtimeEntered[i])
+                {
+                    MessageBox.Show($"Please enter value for Downtime {i + 1}.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            // Calculate average downtime
+            /*  float averageDowntime = serverDowntimes.Sum() / serverDowntimes.Length;
+              averageDowntimeTextBox.Text = "Average downtime score is" + averageDowntime.ToString();
+
+              // Sort the server downtime values
+              float[] sortedDowntimes = serverDowntimes.OrderBy(d => d).ToArray();
+
+              // Display sorted downtime values
+              averageDowntimeTextBox.Clear();
+              foreach (var downtime in sortedDowntimes)
+              {
+                  averageDowntimeTextBox.AppendText("Sorted downtime scores " + downtime.ToString() + Environment.NewLine);
+              }*/
+
+            // Calculate average downtime
+            float averageDowntime = serverDowntimes.Sum() / serverDowntimes.Length;
+
+            // Sort the server downtime values
+            float[] sortedDowntimes = serverDowntimes.OrderBy(d => d).ToArray();
+
+            // Create a string for displaying both average downtime and sorted downtime scores
+            string displayText = $"Average downtime score is {averageDowntime:F1}  Sorted downtime scores ";
+
+            // Append sorted downtime scores to the display text
+            for (int i = 0; i < sortedDowntimes.Length; i++)
+            {
+                displayText += $"{(int)sortedDowntimes[i]}";
+                if (i < sortedDowntimes.Length - 1)
+                {
+                    displayText += " ";
+                }
+            }
+
+            // Set the TextBox text to the display text
+            averageDowntimeTextBox.Text = displayText;
+
+
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void textBox01_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void averageDowntimeTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
