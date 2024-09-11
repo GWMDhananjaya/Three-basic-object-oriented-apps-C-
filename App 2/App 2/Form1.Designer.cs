0b6e30fcfc82ab0d5f69073d0a4c32859cbc6142
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-09-11 21:13:10 +0530

    app 2 makeing

---
+++ App 2/App 2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace App_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.editorViewTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(134, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Reader / Writer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "File Path";
+            // 
+            // logFilePathTextBox
+            // 
+            this.logFilePathTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logFilePathTextBox.Location = new System.Drawing.Point(134, 115);
+            this.logFilePathTextBox.Multiline = true;
+            this.logFilePathTextBox.Name = "logFilePathTextBox";
+            this.logFilePathTextBox.Size = new System.Drawing.Size(306, 46);
+            this.logFilePathTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(156, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Read File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(297, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Write File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // editorViewTextBox
+            // 
+            this.editorViewTextBox.Location = new System.Drawing.Point(134, 264);
+            this.editorViewTextBox.Multiline = true;
+            this.editorViewTextBox.Name = "editorViewTextBox";
+            this.editorViewTextBox.Size = new System.Drawing.Size(306, 270);
+            this.editorViewTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 563);
+            this.Controls.Add(this.editorViewTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.logFilePathTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "File Read Write";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox logFilePathTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox editorViewTextBox;
+    }
+}
+
---
+++ App 2/App 2/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace App_2
+{
+    public partial class Form1 : Form
+    {
+        private string logFilePath;
+        private MyLogFileHandler logFileHandler;
+        public Form1()
+        {
+            InitializeComponent();
+            logFileHandler = new MyLogFileHandler();
+            logFilePathTextBox.Click += LogFilePathTextBox_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LogFilePathTextBox_Click(object sender, EventArgs e)
+        {// Open file dialog to select a file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog.Title = "Select Log File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Set the selected file path in the TextBox
+                logFilePathTextBox.Text = openFileDialog.FileName;
+                logFilePath = openFileDialog.FileName; // Update the log file path
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Read the file contents
+                string fileContents = logFileHandler.ReadFile(logFilePath);
+
+                // Display the file contents in the editor view
+                editorViewTextBox.Text = fileContents;
+            }
+            catch (Exception ex)
+            {
+                // Display error message if an exception occurs
+                MessageBox.Show($"An error occurred while reading the file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Write the contents of the editor view to the file
+                logFileHandler.WriteFile(logFilePath, editorViewTextBox.Text);
+
+                // Display success message
+                MessageBox.Show("File successfully written.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Display error message if an exception occurs
+                MessageBox.Show($"An error occurred while writing to the file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+
+    public class MyLogFileHandler
+    {
+        public string ReadFile(string filePath)
+        {
+            // Read the contents of the file
+            return System.IO.File.ReadAllText(filePath);
+        }
+
+        public void WriteFile(string filePath, string content)
+        {
+            // Write content to the file
+            System.IO.File.WriteAllText(filePath, content);
+        }
+    }
+
+}
